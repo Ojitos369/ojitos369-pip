a5fe1f012ff4243e052801d26f1d9a02d5eccd95
ojitos369 <ojitos369@gmail.com>
2023-06-01 17:00:30 -0600

    add generate_token funtion in utils

--- ojitos369/utils.py
+++ ojitos369/utils.py
@@ -116,6 +116,40 @@ def destructure_d(*args, **kwargs):
     return values
 
 
+def generate_token(length: int = 225, exclude: list = []) -> str:
+    """Generate a random token with a certain length and excluding some characters
+        Args:
+            length (int, optional): Length of the token. Defaults to 225.
+            exclude (list, optional): List of characters to exclude. Defaults to [].
+        Returns:
+            str: Token
+    """
+    if type(exclude) == str:
+        exclude = list(exclude)
+    exclude += ['"', "'", '\\', '`', ';', ',', ' ', '']
+    # Import lyrics, numbers and symbols
+    from string import ascii_letters, digits, punctuation
+    import random
+    
+    for e in exclude:
+        ascii_letters = ascii_letters.replace(e, '')
+        digits = digits.replace(e, '')
+        punctuation = punctuation.replace(e, '')
+    
+    max_length = max(len(ascii_letters), len(digits), len(punctuation))
+    ascii_letters = (ascii_letters * (int(max_length // len(ascii_letters) ) + 1))[0:max_length]
+    digits = (digits * (int(max_length // len(digits) ) + 1))[0:max_length]
+    punctuation = (punctuation * (int(max_length // len(punctuation) ) + 1))[0:max_length]
+    
+    options = ascii_letters + digits + punctuation
+    options = options
+    # pln(f'options: {options}')
+    
+    token = ''
+    for _ in range(length):
+        token += random.choice(options)
+    # pln(token)
+    return token
 
 
 
